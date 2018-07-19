ddb21b2cfe5a27c3d38ab1d30a89e76bd2eb738e
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-19 23:59:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,28 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push({itemName: item, itemPrice: Math.floor((Math.random()*100)+1)});
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  if(cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  }
+  else {
+      var output = "In your cart, you have ";
+      for(var i=0; i<cart.length; i++) { 
+        if(i === cart.length-1) {
+          output += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+          }
+        else {
+          output += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+          }
+        }
+      return output;
+    }
+
 }
 
 function total() {

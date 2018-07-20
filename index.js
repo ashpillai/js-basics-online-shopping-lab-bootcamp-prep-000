de1444d00c9098df9cc33df41b8e6c36b25c409c
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-20 00:55:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}. ${setCart([])}`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.${setCart([])}`
   }
 
  

840bab3e0446ef3e5a7fecffb5029010d2d2a171
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-20 00:42:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if(cardNumber === undefined) {
+  if(cardNumber === "") {
     console.log("Sorry, we don't have a credit card on file for you.");
   }
   else {
